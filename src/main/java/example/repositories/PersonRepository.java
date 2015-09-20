86da930daad91d04b0bdd604bf5b8f56e3f31e5e
Aaron Burrell <aaronburrell@gmail.com>
2015-09-20 02:41:11 -0400

    Added REST Repo that creates a /people services

---
+++ src/main/java/example/repositories/PersonRepository.java
@@ -0,0 +1,18 @@
+package example.repositories;
+
+/**
+ * Created by aaronburrell on 9/20/15.
+ */
+import java.util.List;
+
+import example.domain.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
